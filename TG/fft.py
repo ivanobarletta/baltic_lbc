bf062226554643092c0f9116f6b6ad5ac1ff9612
ivanobarletta <ivano.barletta@gmail.com>
2024-04-03 12:01:53 +0200

    bug fix cycles per day

--- TG/fft.py
+++ TG/fft.py
@@ -22,9 +22,10 @@ N       = len(fft_ssh)
 n       = np.arange(N)
 
 # sampling rate
-dt      = (time[1]-time[0]) / np.timedelta64(1,"s")
-sr      = 1. / dt 
+dt      = (time[1]-time[0]) / np.timedelta64(1,"s") # dt in seconds
+sr      = 1. / dt                                   
 
+# time span
 T       = N / sr
 freq    = n / T 
 
@@ -33,11 +34,15 @@ n_oneside   = N // 2
 
 f_oneside   =  freq[:n_oneside]
 
+# periods
 t_days      = 1/f_oneside / 86400.0
+# convert in cycles per day
+cycles_per_day  = 1.0 / t_days
+
 
 # make plot
 plt.figure()
-plt.loglog(t_days, np.abs(fft_ssh[:n_oneside])/n_oneside)
+plt.loglog(cycles_per_day, np.abs(fft_ssh[:n_oneside])/n_oneside)
 plt.xlabel("cycles per day")
 plt.grid()
 
