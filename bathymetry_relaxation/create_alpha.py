1863de099a9715ce9a73ba04780bef7acf5d3a72
ivanobarletta <ivano.barletta@gmail.com>
2024-04-17 17:10:57 +0200

    comments added

--- bathymetry_relaxation/create_alpha.py
+++ bathymetry_relaxation/create_alpha.py
@@ -2,8 +2,44 @@ import xarray as xr
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-neatlBathyPath  = "bathy_meter2.nc"
+#
+#   nx = 1093
+#   
+#   i-indexes in python notation
+#   
+#       
+#        |                           EAST2 LBC BUFFER ZONE                         | Last Value 
+#        |                                                                         | (masked)    
+#   
+#   1076 1077 1078 1079 1080 1081 1082 1083 1084 1085 1086 1087 1088 1089 1090 1091 1092 
+#   
+#          o    o    o    o    o    o    o    o    o    o    o    o    o    o    o    o   
+#   
+#          
+#   xidx0 = 1077
+#   xidx1 = 1091
+#          
+#   search for a linear function f[]:
+#       f = m * x + q 
+#       with:   
+#           f[xidx0] = f[1077] = 1
+#           f[xidx1] = f[1091] = 0    
+#   
+#   
+#   slope of the linear function:
+#       m = 1 / (xidx1 - xidx0 ) = 1. / 14
+#   
+#   y at origin:
+#       f must be 0 in i=xidx1
+#   
+#       0 = m * xidx1 + q
+#   
+#       q = -m * xidx1
+#   
+
+neatlBathyPath  = "bathy_meter2.nc" # bathy_meter2.nc data is exactly as in bathy_meter.nc 
 outDataset      = "alpha.nc"
+
 
 # open dataset and get sizes
 dsNeatl = xr.open_dataset(neatlBathyPath)
